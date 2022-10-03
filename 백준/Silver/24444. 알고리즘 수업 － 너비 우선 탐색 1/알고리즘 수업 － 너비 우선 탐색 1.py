297dd6be492682160ec0d4702e0b8130453996eb
김준영 <45484403+ats166@users.noreply.github.com>
2022-10-03 23:55:28 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 652 ms, Memory: 58112 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N,M,R = map(int,input().split())
+
+arr =[[] for _ in range(N)]
+used = [0] * N
+for i in range(M):
+    a,b = map(int,input().split())
+
+    arr[a-1].append(b-1)
+    arr[b-1].append(a-1)
+
+def bfs(now):
+    cnt = 1
+    q = deque([arr[now]])
+    while q:
+        queue = q.popleft()
+        queue.sort()
+
+        for i in queue:
+            if used[i] == 0:
+                cnt += 1
+                used[i] = cnt
+                q.append(arr[i])
+
+used[R-1] = 1
+bfs(R-1)
+for i in range(N):
+    print(used[i])
