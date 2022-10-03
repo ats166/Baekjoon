1a8feb65a863e138711621ca7660c3141824126f
김준영 <45484403+ats166@users.noreply.github.com>
2022-10-03 22:25:13 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 820 ms, Memory: 162316 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(200000)
+input = sys.stdin.readline
+
+N,M,R = map(int,input().split())
+
+arr = [[] for _ in range(N)]
+used = [0] * N
+cnt = 0
+
+for i in range(M):
+    a,b = map(int,input().split())
+
+    arr[a-1].append(b-1)
+    arr[b-1].append(a-1)
+
+def dfs(node):
+    global cnt
+    cnt += 1
+    used[node] = cnt
+    arr[node] = sorted(arr[node], reverse=True)
+    for k in arr[node]:
+        if used[k] == 0:
+            dfs(k)
+
+
+dfs(R-1)
+for i in range(N):
+    print(used[i])
