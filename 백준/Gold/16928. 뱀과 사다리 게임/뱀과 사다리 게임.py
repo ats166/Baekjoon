b265628fb6f0164d74427831c2be150c4a6444e5
김준영 <45484403+ats166@users.noreply.github.com>
2022-10-05 22:07:21 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 88 ms, Memory: 30972 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -3,15 +3,11 @@ import copy
 N,M = map(int,input().split())
 arr = [-1] * 101
 tmp = [-1] * 101
-arr[1] = 0
 acc = [False] * 101
+arr[1] = 0
 cnt,flag = 0,0
 
-for _ in range(N):
-    a,b = map(int,input().split())
-    acc[a] = b
-
-for _ in range(M):
+for _ in range(N+M):
     a,b = map(int,input().split())
     acc[a] = b
 
@@ -21,17 +17,16 @@ while True:
         break
 
     cnt += 1
+    
     for j in range(101):
         if arr[j] == cnt-1:
             for i in range(1,7):
                 if j+i > 100:
-                    flag = 1
                     break
                 if acc[j+i] == False:
                     tmp[j+i] = cnt
                 else:
                     tmp[acc[j+i]] = cnt
-        if flag == 1:
-            break
+                    
     arr = copy.deepcopy(tmp)
     tmp = [-1] * 101
