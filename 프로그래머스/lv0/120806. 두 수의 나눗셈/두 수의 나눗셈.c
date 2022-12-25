d515ad5f1020f37e48f19f25b00fd13025e6a25a
김준영 <45484403+ats166@users.noreply.github.com>
2022-12-25 14:30:16 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    double answer = 0;
+    
+    answer = (double)num1 / (double)num2 * (double)1000;
+    
+    return answer;
+}
