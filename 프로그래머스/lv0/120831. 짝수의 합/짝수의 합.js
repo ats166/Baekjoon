e4dc639c5d7b8227fdc718e2c1dadaa813e642e4
김준영 <45484403+ats166@users.noreply.github.com>
2022-12-30 22:06:24 +0900

    [level 0] Title: 짝수의 합, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = 0;
+    let arr = [];
+
+    for (let i = 0; i < n+1; i++) {
+        arr[i] = i;
+        if (arr[i] % 2 == 0) {
+            answer += arr[i]
+        }
+    }
+    
+    return answer;
+}
