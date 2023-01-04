10d115341e51d670cd01b7a6a88dcc33c9b25399
김준영 <45484403+ats166@users.noreply.github.com>
2023-01-04 22:22:52 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,15 @@
+function solution(price) {
+    var answer = 0;
+    
+    if (price >= 500000) {
+        price -= (price*0.2)
+    } else if (price >= 300000) {
+        price -= (price*0.1)
+    } else if (price >= 100000) {
+        price -= (price*0.05)
+    }
+    
+    
+    
+    return parseInt(price);
+}
