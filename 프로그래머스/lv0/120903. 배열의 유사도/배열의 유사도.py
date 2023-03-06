cc756379b274c4160fde0cd7dac40c2444be7da7
김준영 <45484403+ats166@users.noreply.github.com>
2023-03-07 08:55:50 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
@@ -0,0 +1,8 @@
+def solution(s1, s2):
+    answer = 0
+    
+    for j in range(len(s1)):
+        for i in range(len(s2)):
+            if s1[j] == s2[i]:
+                answer += 1
+    return answer
