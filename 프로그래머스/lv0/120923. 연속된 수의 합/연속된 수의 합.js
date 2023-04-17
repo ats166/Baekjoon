c8ccb2c428c533d0d032cd544f9e64ae59f98b75
김준영 <45484403+ats166@users.noreply.github.com>
2023-04-17 23:48:01 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -0,0 +1,31 @@
+function solution(num, total) {
+  var answer = [];
+  let isEven, standard;
+
+  if (num % 2 == 0) {
+    isEven = true;
+  } else {
+    isEven = false;
+  }
+
+  if (isEven) {
+    standard = (total - num / 2) / num;
+  } else {
+    standard = total / num;
+  }
+
+  if (isEven) {
+    for (let i = standard - num / 2 + 1; i <= standard + num / 2; i++) {
+      answer.push(i);
+    }
+  } else {
+    for (
+      let i = standard - Math.floor(num / 2);
+      i <= standard + Math.floor(num / 2);
+      i++
+    ) {
+      answer.push(i);
+    }
+  }
+  return answer;
+}
