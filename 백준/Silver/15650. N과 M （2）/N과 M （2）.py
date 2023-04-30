813bb32acd177cfef9da865b29291433b3d30f6e
김준영 <45484403+ats166@users.noreply.github.com>
2023-04-30 20:53:53 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,19 +1,19 @@
-N,M = map(int,input().split())
-used = [0] * (N+1)
+N, M = map(int,input().split())
 path = [0] * M
+used = [0] * N
 
 def dfs(st,level):
 
     if level == M:
-        print(*path,end =' ')
-        print()
+        print(*path, sep=' ')
         return
 
-    for i in range(st,N+1):
+    for i in range(st,N):
         if used[i] == 0:
             used[i] = 1
-            path[level] = i
-            dfs(i+1,level+1)
+            path[level] = i + 1
+            dfs(i+1, level+1)
             used[i] = 0
 
-dfs(1,0)
+
+dfs(0,0)
