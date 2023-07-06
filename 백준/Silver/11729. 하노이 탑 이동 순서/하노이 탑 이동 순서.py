a18f9d532689464e5b478571b14486393205c2bb
김준영 <45484403+ats166@users.noreply.github.com>
2023-07-06 18:11:44 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 972 ms, Memory: 47980 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,21 +1,23 @@
-def hanoi(N, start, end, helper):
+N = int(input())
+arr = []
+cnt = 0
+
+def hanoi(N,start,end,helper):
     global cnt
-    global new_list
+
     if N == 1:
-        new_list.append(start)
-        new_list.append(end)
+        arr.append(start)
+        arr.append(end)
         cnt += 1
         return
-    hanoi(N-1, start, helper, end)
-    new_list.append(start)
-    new_list.append(end)
-    cnt +=1
-    hanoi(N-1, helper, end, start)
 
-new_list = []
-cnt = 0
-hanoi(int(input()),1,3,2)
+    hanoi(N-1,start,helper,end)
+    arr.append(start)
+    arr.append(end)
+    cnt += 1
+    hanoi(N-1,helper,end,start)
 
+hanoi(N,1,3,2)
 print(cnt)
-for i in range(0,len(new_list),2):
-    print (f'{new_list[i]} {new_list[i+1]}')
+for i in range(0,len(arr),2):
+    print(f'{arr[i]} {arr[i+1]}')
