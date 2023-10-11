5ac0af503a28998b3bb8c13fc1da01d605f44ed0
김준영 <45484403+ats166@users.noreply.github.com>
2023-10-12 00:00:51 +0900

    [Gold IV] Title: 단어 수학, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.py
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -2,8 +2,7 @@ import sys
 
 n = int(sys.stdin.readline())
 
-alphabet_dict = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}
-alphabet_list = []
+alphabet_dict = {}
 answer = 0
 pocket = []
 
@@ -12,16 +11,16 @@ for _ in range(n):
     pocket.append(alphabet)
 
 for alphabet in pocket:
-    for i in range(len(alphabet)):
+    for i, char in enumerate(alphabet):
         num = 10 ** (len(alphabet) - i - 1)
-        alphabet_dict[alphabet[i]] += num
+        if char not in alphabet_dict:
+            alphabet_dict[char] = 0
+        alphabet_dict[char] += num
 
-for value in alphabet_dict.values():
-    if value > 0:
-        alphabet_list.append(value)
-
+alphabet_list = [value for value in alphabet_dict.values() if value > 0]
 sorted_list = sorted(alphabet_list, reverse=True)
-for i in range(len(sorted_list)):
-    answer += sorted_list[i] * (9 - i)
+
+for i, value in enumerate(sorted_list):
+    answer += value * (9 - i)
 
 print(answer)
